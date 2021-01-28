66db9dd37b0555b4a24595038df0663039a950f5
John Cunningham <john.cunningham.design@gmail.com>
2021-01-28 22:47:48 +0000

    Initial Commit

---
+++ FitnessTrackerApp/AVCaptureVideoOrientation+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  AVCaptureVideoOrientation+Extension.swift
+//  VisionPose
+//
+//  Created by qubsys on 17/01/2021.
+//
+
+import AVFoundation
+import UIKit
+
+extension AVCaptureVideoOrientation {
+    init(deviceOrientation: UIDeviceOrientation) {
+        switch deviceOrientation {
+        case .landscapeLeft:
+            self = .landscapeLeft
+        case .landscapeRight:
+            self = .landscapeRight
+        case .portrait:
+            self = .portrait
+        case .portraitUpsideDown:
+            self = .portraitUpsideDown
+        default:
+            self = .portrait
+        }
+    }
+}
